4353ad75c6f8b0b3d4dd454f60bee9ff7fbc8c32
Lavkush Varma <140130429+Lavkush3844@users.noreply.github.com>
2024-02-22 10:37:22 +0530

    Delete User_Side directory

--- User_Side/src/service/const.js
+++
@@ -1,8 +0,0 @@
-export const GETDATAREQ = "GETDATAREQ"
-export const GETDATARES = "GETDATARES"
-export const GETDATAREJ = "GETDATAREJ"
-export const ADDITEM = 'ADDITEM';
-export const DELITEM = 'DELITEM';
-export const INC = 'INC';
-export const DSC = 'DSC';
-
--- User_Side/src/service/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { ProductsReducer } from "./products.reducer";
-import cartReducer from "./addCart.reducer";
-
-export const rootReducer = combineReducers({
-    ProductsReducer,
-    cartReducer
-})
